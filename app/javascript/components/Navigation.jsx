099952635c8d8f77c8e18b41a70b801c69cbdd98
Luis Mercado <luis.mercado@sleepnumber.com>
2026-01-05 13:07:45 -0500

    env usage

--- app/javascript/components/Navigation.jsx
+++ app/javascript/components/Navigation.jsx
@@ -6,6 +6,7 @@ export default function Navigation() {
     <nav className='nav'>
       <div className='nav-brand'>Luis Mercado</div>
       <ul className='nav-links'>
+        <li><NavLink to='/'>Home</NavLink></li>
         <li><NavLink to='/drum-and-bass'>Drum & Bass</NavLink></li>
         <li><NavLink to='/software-engineering'>Software Engineering</NavLink></li>
         <li><NavLink to='/wrestling'>Wrestling</NavLink></li>
