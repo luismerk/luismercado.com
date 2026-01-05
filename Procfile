b4c7a2ae7711cc81090cfd2f8e2e738a11bd62b8
Luis Mercado <luis.mercado@sleepnumber.com>
2026-01-05 16:58:53 -0500

    try this

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: RAILS_ENV=production bundle exec puma -C config/puma.rb -p $PORT
+web: RAILS_ENV=production bundle exec puma -C config/puma.rb
--- config/puma.rb
+++ config/puma.rb
@@ -1,17 +1,13 @@
-# Bind to EB's $PORT (Amazon Linux 2023 expects TCP)
-port ENV.fetch("PORT") { 3000 }
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+min_threads_count = max_threads_count
+threads min_threads_count, max_threads_count
 
-# Rails environment
-environment ENV.fetch("RAILS_ENV") { "production" }
+workers ENV.fetch("WEB_CONCURRENCY", 1)
 
-# Workers and threads (safe for small instances)
-workers ENV.fetch("WEB_CONCURRENCY") { 1 }
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
-
-# Preload app for performance
 preload_app!
 
-# Do NOT bind to a Unix socket
-# Remove any old lines like: bind "unix:///var/run/puma/my_app.sock"
-# EB handles the web server proxying to Puma via TCP on $PORT
+# THIS IS THE IMPORTANT PART
+port ENV.fetch("PORT", 3000)
+environment ENV.fetch("RAILS_ENV", "production")
+
+plugin :tmp_restart
