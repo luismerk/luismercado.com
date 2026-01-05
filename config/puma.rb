81bc0e534c9704bbf1846b1ae5cc779c29384da2
Luis Mercado <luis.mercado@sleepnumber.com>
2026-01-05 14:43:37 -0500

    oops

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@ port ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Recommended for EB:
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 1 }
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
