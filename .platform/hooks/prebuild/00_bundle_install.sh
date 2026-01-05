0f30a80fd9c807a71be7a5083694e8ce4a6f7c18
Luis Mercado <luis.mercado@sleepnumber.com>
2026-01-05 12:30:16 -0500

    different approach

---
+++ .platform/hooks/prebuild/00_bundle_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Ensure bundler installs gems before starting app
+echo "Running bundle install..."
+bundle install --deployment --without development test
+echo "Bundle install completed."
