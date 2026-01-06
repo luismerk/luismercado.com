3145d641abe9de864301d155cc515953e0925a46
Luis Mercado <luis.mercado@sleepnumber.com>
2026-01-06 17:36:41 -0500

    add hamburger menu

--- app/javascript/components/Navigation.jsx
+++ app/javascript/components/Navigation.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default function Navigation() {
+  const [open, setOpen] = useState(false);
+
   return (
     <header className='site-header'>
       <nav className='nav-container'>
         <div className='nav-brand'>Luis Mercado</div>
-        <ul className='nav-links'>
+
+        <button
+          className='nav-toggle'
+          onClick={() => setOpen(!open)}
+          aria-label='Toggle navigation'
+          aria-expanded={open}
+        >
+          <span/>
+          <span/>
+          <span/>
+        </button>
+
+        <ul className={`nav-links ${open ? "open" : ""}`}>
           <li><NavLink to='/'>Home</NavLink></li>
           <li><NavLink to='/drum-and-bass'>Drum & Bass</NavLink></li>
           <li><NavLink to='/software-engineering'>Software Engineering</NavLink></li>
