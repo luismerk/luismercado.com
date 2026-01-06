c4127a9fb4d8704677a0db0abe1fc8d92d064d81
Luis Mercado <luis.mercado@sleepnumber.com>
2026-01-06 14:57:37 -0500

    do this too

--- .platform/hooks/predeploy/01_create_socker_dir.sh
+++ .platform/hooks/predeploy/01_create_socker_dir.sh
@@ -3,3 +3,8 @@ mkdir -p /var/run/puma
 chown webapp:webapp /var/run/puma
 chmod 755 /var/run/puma
 echo "Created /var/run/puma directory with appropriate permissions."
+
+mkdir -p /var/pids
+chown webapp:webapp /var/pids
+chmod 755 /var/pids
+echo "Created /var/pids directory with appropriate permissions."
