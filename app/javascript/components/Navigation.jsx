35490417a0752eec9c3eef44119423f89fbe0352
Luis Mercado <luis.mercado@sleepnumber.com>
2026-01-06 17:27:15 -0500

    much better styles

--- app/javascript/components/Navigation.jsx
+++ app/javascript/components/Navigation.jsx
@@ -3,15 +3,17 @@ import { NavLink } from 'react-router-dom';
 
 export default function Navigation() {
   return (
-    <nav className='nav'>
-      <div className='nav-brand'>Luis Mercado</div>
-      <ul className='nav-links'>
-        <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/drum-and-bass'>Drum & Bass</NavLink></li>
-        <li><NavLink to='/software-engineering'>Software Engineering</NavLink></li>
-        <li><NavLink to='/wrestling'>Wrestling</NavLink></li>
-        <li><NavLink to='/physics'>Physics</NavLink></li>
-      </ul>
-    </nav>
+    <header className='site-header'>
+      <nav className='nav-container'>
+        <div className='nav-brand'>Luis Mercado</div>
+        <ul className='nav-links'>
+          <li><NavLink to='/'>Home</NavLink></li>
+          <li><NavLink to='/drum-and-bass'>Drum & Bass</NavLink></li>
+          <li><NavLink to='/software-engineering'>Software Engineering</NavLink></li>
+          <li><NavLink to='/wrestling'>Wrestling</NavLink></li>
+          <li><NavLink to='/physics'>Physics</NavLink></li>
+        </ul>
+      </nav>
+    </header>
   );
 }
