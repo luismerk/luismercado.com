43a49caf78d973637ca59f8f2b30d54c3aec3f76
Luis Mercado <luis.mercado@sleepnumber.com>
2026-01-24 15:39:17 -0500

    get fast refresh working

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,29 @@
 import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import React from '@vitejs/plugin-react-swc'
+import Rails from 'vite-plugin-rails'
 
 export default defineConfig({
-  root: "app/javascript",
-  plugins: [RubyPlugin()],
+  root: 'app/javascript',
+  plugins: [
+    Rails({
+      fullReload: {
+        additionalPaths: ['config/routes.rb', 'app/views/**/*'],
+        delay: 200,
+      },
+    }),
+    React(),
+  ],
   server: {
     host: 'localhost',
-    port: 3036,       // matches the port your bin/dev logs
+    port: 3036,
     hmr: {
+      protocol: 'ws',
       host: 'localhost',
-      protocol: 'ws',  // usually ws for WebSocket
       port: 3036
     }
+  },
+  build: {
+    outDir: '../../public/vite',
+    emptyOutDir: true
   }
 })
